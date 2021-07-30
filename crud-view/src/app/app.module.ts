3055284d06f3aea5074754e2924eddc4dbfaa704
asmaahassnasr <asmaa14has@gmail.com>
2021-07-30 12:33:37 +0200

    Creating project structure com+serv

--- crud-view/src/app/app.module.ts
+++ crud-view/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryComponent } from './country/country.component';
+import { ShowCountComponent } from './country/show-count/show-count.component';
+import { AddEditComponent } from './country/add-edit/add-edit.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryComponent,
+    ShowCountComponent,
+    AddEditComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
