891ccc7ec402b9446fa64f3871f83adb32831cde
asmaahassnasr <asmaa14has@gmail.com>
2021-07-30 03:18:06 +0200

    Creating Country Model

---
+++ CRUD_APIs/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_APIs.Models
+{
+    public class Country
+    {
+        [Key]
+        public int CountryId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar (50)")]
+        public string CountryName { get; set; }
+        [Required]
+        [StringLength(2)]
+        [Column(TypeName = "nvarchar (2)")]
+        public string CountryCode { get; set; }
+    }
+}
