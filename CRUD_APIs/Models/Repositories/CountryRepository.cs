b5ac26505c711dc3f19edbebfa052f5ee9a01dec
asmaahassnasr <asmaa14has@gmail.com>
2021-07-30 04:34:57 +0200

    Creating Country and CountryRepo Repositories

---
+++ CRUD_APIs/Models/Repositories/CountryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_APIs.Models.Repositories
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public CountryRepository(AppDbContext _appDbContext)
+        {
+            appDbContext = _appDbContext;
+        }
+
+        public async Task<IEnumerable<Country>> GetCountries()
+        {
+            return await (appDbContext.Countries.ToListAsync());
+        }
+
+
+
+        public async Task<Country> GetCountryById(int id)
+        {
+
+            return await appDbContext.Countries.FirstOrDefaultAsync(c => c.CountryId == id);
+        }
+    }
+}
---
+++ CRUD_APIs/Models/Repositories/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_APIs.Models.Repositories
+{
+  public  interface ICountryRepository
+    {
+        Task<IEnumerable<Country>> GetCountries();
+        Task<Country> GetCountryById(int id);
+    }
+}
