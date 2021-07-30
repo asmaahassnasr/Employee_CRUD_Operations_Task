db4127fcb5ca723b8aa8122057984f597464e8c4
asmaahassnasr <asmaa14has@gmail.com>
2021-07-30 04:57:41 +0200

    Creating Employee Controller and GetAllEmployees API

---
+++ CRUD_APIs/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CRUD_APIs.Models.Repositories;
+using CRUD_APIs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeesController(IEmployeeRepository _employeeRepository)
+        {
+            employeeRepository = _employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                // Ok : status code => 200 
+                return Ok(await employeeRepository.GetEmployees());
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Retreiving data from the server");
+            }
+        }
+    }
+}
